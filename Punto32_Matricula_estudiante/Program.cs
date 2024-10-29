2e71be5ee4426f2d6682de0f726ddd990e1a16f6
CamiloVelezL <105832950+CamiloVelezL@users.noreply.github.com>
2024-10-28 20:40:45 -0500

    primer commit con el ejercicio planteado

---
+++ Punto32_Matricula_estudiante/Program.cs
@@ -0,0 +1,27 @@
+﻿/*32.Se desea obtener el valor de la matrícula de un estudiante cuyo valor se calcula de la siguiente manera:
+
+
+1· Si toma 20 o menos créditos, paga el crédito al valor normal.
+2· Si toma por encima de 20 créditos, se pagarán los créditos extras al doble de valor normal.
+3· Si el estudiante es de estrato 1, 2 o 3 recibe los siguientes descuentos:
+   - Si el estrato es 1, el descuento es del 80%.
+   - Si el estrato es 2, el descuento es del 50%.
+   - Si el estrato es 3, el descuento es del 30%.
+
+Además, los estratos 1 y 2 reciben subsidio de alimentación y transporte de la siguiente manera:
+
+1· Para el estrato 1, el subsidio de alimentación y transporte es igual a $200.000.
+2· Para el estrato 2, el subsidio de alimentación y transporte es igual a $100.000.
+
+Se debe informar al usuario sobre el costo de la matrícula y el valor del subsidio.
+
+La aplicación debe preguntar al usuario si quiere volver al inicio o no para calcular la matrícula de un nuevo estudiante.
+Hacer uso de todos los elementos de programación que hemos visto hasta el momento, incluyendo POO (clases, propiedades, métodos)*/
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+    }
+}
