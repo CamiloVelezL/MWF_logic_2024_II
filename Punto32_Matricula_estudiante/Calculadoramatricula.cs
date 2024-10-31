78220653a24a057a061b7161b6e895b78fb430af
CamiloVelezL <105832950+CamiloVelezL@users.noreply.github.com>
2024-10-30 19:18:37 -0500

    commit cuatro se calculan los costos por del objeto estudiante con los metodos Costo, Descuento y subsidio

--- Punto32_Matricula_estudiante/Calculadoramatricula.cs
+++ Punto32_Matricula_estudiante/Calculadoramatricula.cs
@@ -1,15 +1,64 @@
 ﻿/*Se creo esta clase para realizar el calculo de matriclula, descuentos y subsidios que aplican a cada estudiamte para mantener
 el (main) lo mas limpio posible */
 
+using MatriculaEestudiante;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Punto32_Matricula_estudiante
+namespace Matricula_estudiante // la encapsulacion esta dentro de la clase Calculadoramatricula.
 {
-    internal class Calculadoramatricula
-    {
+    public class Calculadoramatricula
+    {                 //CONSTANTES
+        private const int LimiteCreditos = 20;
+        private const decimal CreditosExtras = 2.0m;
+                      //METODO MATRICULA
+        public decimal CalcularMatricula (Estudiante estudiante, decimal valorCredito)
+        {
+            decimal CostoBase;
+            if (estudiante.Creditos <= LimiteCreditos)
+            {
+                CostoBase = estudiante.Creditos * valorCredito;
+            }
+            else
+            {
+                CostoBase = (LimiteCreditos * valorCredito) + ((estudiante.Creditos - LimiteCreditos) * valorCredito * CreditosExtras);
+            }
+            decimal descuento = CalcularDescuento(estudiante.Estrato);
+            return CostoBase * (1 - descuento);
+            }
+                    //METODO DESCUENTO
+        public decimal CalcularDescuento(int Estrato)
+        {
+            switch (Estrato)
+            {
+                case 1:
+                    return 0.80m;
+                case 2:
+                    return 0.50m;
+                case 3:
+                    return 0.30m;
+                default:
+                    return 0m;
+
+            }
+        }
+                   //METODO SUBSIDIO
+        public decimal CalcularSubsidio(int Estrato)
+        {
+            switch (Estrato)
+            {
+                case 1:
+                    return 200000m;
+                case 2:
+                    return 100000m;
+                default:
+                    return 0m;
+            }
+        }
+        
     }
+
 }
